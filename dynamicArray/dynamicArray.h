c82f0a587233c79c28fa6ac4ab527545cebaf02d
yexinran1 <yexinran96@163.com>
2023-12-18 14:13:33 +0800

    [修改]动态数组初始化函数接口完成

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -13,7 +13,7 @@ typedef struct dynamicArray
 
 /* API：application program interface*/
 /* 动态数组初始化 */
-int dynamicArrayInit(dynamicArray *pArray);
+int dynamicArrayInit(dynamicArray *pArray, int capacity);
 
 /* 动态数组插入数据(默认插到数组的末尾) */
 int dynamicArrayInsertData(dynamicArray *pArray, ELEMENTTYPE val);
