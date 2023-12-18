0b05a414617301247176d6b518aaaa7d6efd5f3c
yexinran1 <yexinran96@163.com>
2023-12-18 15:18:03 +0800

    [修改]完成动态数组插入的API接口

--- dynamicArray/dynamicArray.c
+++ dynamicArray/dynamicArray.c
@@ -9,10 +9,13 @@ enum STATUS_CODE
     ON_SUCCESS,
     NULL_PTR,
     MALLOC_ERROR,
-
+    INVALID_ACCESS,
 };
 
 #define DEFAULT_SIZE 10
+
+/* 静态函数前置声明 */
+static int expandDynamicCapacity(dynamicArray *pArray);
 
 /* API：application program interface*/
 /* 动态数组初始化 */
@@ -28,13 +31,13 @@ int dynamicArrayInit(dynamicArray *pArray, int capacity)
         capacity = DEFAULT_SIZE;
     }
     /* 分配空间 */
-    pArray->data = (ELEMENTTYPE*)malloc(sizeof(ELEMENTTYPE)*capacity);
-    if(pArray->data ==NULL)
+    pArray->data = (ELEMENTTYPE*)malloc(sizeof(ELEMENTTYPE) * capacity);
+    if(pArray->data == NULL)
     {
         return MALLOC_ERROR;
     }
     /* 清除脏数据 */
-    memset(pArray->data, 0, sizeof(ELEMENTTYPE)*capacity);
+    memset(pArray->data, 0, sizeof(ELEMENTTYPE) * capacity);
     /* 初始化动态数组的参数属性 */
     pArray->len = 0;
     pArray->capacity = capacity;
@@ -45,14 +48,73 @@ int dynamicArrayInit(dynamicArray *pArray, int capacity)
 /* 动态数组插入数据(默认插到数组的末尾) */
 int dynamicArrayInsertData(dynamicArray *pArray, ELEMENTTYPE val)
 {
-
+    return dynamicArrayAppointPosInsertData(pArray, pArray->len, val);
 }
 
+static int expandDynamicCapacity(dynamicArray *pArray)
+{
+    int ret = 0;
+    int needExpandCapacity = pArray->capacity + (pArray->capacity + 1);
+    /* 备份指针 */
+    ELEMENTTYPE *tmpPtr = pArray->data;
+    pArray->data = (ELEMENTTYPE*)malloc(sizeof(ELEMENTTYPE) * needExpandCapacity);
+    if(pArray->data == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 把之前的数据全部拷贝过来 */
+    for(int idx = 0; idx< pArray->len; idx++)
+    {
+        pArray->data[idx] = tmpPtr; 
+    }
+
+    /* 释放以前的内存， 避免内存泄漏 */
+    if(tmpPtr != NULL)
+    {
+        free(tmpPtr);
+        tmpPtr = NULL;
+    }
+    pArray->capacity = needExpandCapacity;
+    return ret;
+}
 
 /* 动态数组插入数据，在指定位置插入 */
 int dynamicArrayAppointPosInsertData(dynamicArray *pArray, int pos, ELEMENTTYPE val)
 {
+    if(pArray == NULL)
+    {
+        return NULL_PTR;
+    }
 
+    /* 判断位置的合法性 */
+    if(pos < 0 || pos > pArray->len)
+    {
+        return INVALID_ACCESS;
+    }
+    #if 1
+    /* 数组扩容的临界值是：数组大小的1.5倍 >=数组容量 */
+    if((pArray->len + (pArray->len >> 1)) < pArray->capacity)
+    {
+        /* 开始扩容 */
+        expandDynamicCapacity(pArray);
+    }
+    #endif
+#if 1
+    /* 数据后移，流出pos位置插入 */
+    for(int idx = pArray->len; idx > pos; idx--)
+    {
+        pArray->data[idx] = pArray->data[idx - 1];
+    }
+#else
+    for(int idx = pos; idx < pArray->len; idx++)
+    {
+        pArray->data[idx + 1] = pArray->data
+    }
+#endif
+    /* 找到对应的值，填入到数组中 */
+    pArray->data[pos] = val;
+    /* 数组的大小加一 */
+    (pArray->len) ++;
 }
 
 
