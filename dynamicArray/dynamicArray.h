195e979848d34852761897e87caa116a94385d31
yexinran1 <yexinran96@163.com>
2023-12-18 11:12:40 +0800

    [新增]动态数组的头文件.h

---
+++ dynamicArray/dynamicArray.h
@@ -0,0 +1,7 @@
+#ifndef _DYNAMIC_ARRAY_H_
+#define _DYNAMIC_ARRAY_H_
+
+/* 避免头文件重复包含 */
+
+
+#endif
---
+++ dynamicArray/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"dynamicArray.h"
+
+int main()
+{
+    /* 静态数组 
+        1、不够灵活
+        2、可能会有浪费的现象
+    */
+    int classStu[30];
+    
+    return 0;
+}
