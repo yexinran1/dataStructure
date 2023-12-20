a8b8111ddf212f6d67ee7bd613f85c721c363106
yexinran1 <yexinran96@163.com>
2023-12-20 16:26:51 +0800

    [修改]完善删除API接口， 更新为指针

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -143,15 +143,28 @@ int LinkListDelAppointPos(LinkList *pList, int pos)
     LinkNode * travelNode = pList->head;
 #else
 #endif
+    int flag = 0;
+    /* 需要修改尾指针*/
+    if(pos == pList->len)
+    {
+        flag = 1;
+    }
+    LinkNode * needDelNode = NULL;
     while(--pos)
     {
         /* 向后移动位置 */
         travelNode = travelNode->next;
         //pos--;
     }
+    
     //跳出循环找到的是哪一个结点 
     LinkNode * needDelNode = travelNode->next;;
     travelNode->next = needDelNode->next;
+    if(flag)
+    {
+        /* 调整尾指针 */
+        pList->tail = travelNode;
+    }
 
     /* 释放内存 */
     if(needDelNode != NULL)
